8dfb5c189743af4f195436eb3620dd3a19a704bd
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-02-19 18:42:25 -0500

    clase 12 terminada, se agregó un Header

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from '../containers/Layout.jsx';
+import Login from '../containers/Login.jsx';
+import RecoveryPassword from '../containers/RecoveryPassword.jsx';
+import Home from '../pages/Home.jsx';
+import NotFound from '../pages/NotFound.jsx';
 import '../styles/global.css';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Layout>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/login" component = {Login} />
-                    <Route exact path="recovery-password" component={RecoveryPassword} />
-                    <Route component={NotFound} />
-                </Layout>
-            </Switch>     
+            <Layout>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/login" element = {<Login />} />
+                    <Route exact path="recovery-password" element={<RecoveryPassword />} />
+                    <Route path= "*" element={<NotFound />} />
+                </Routes>
+            </Layout>     
         </BrowserRouter>
     );
 }
