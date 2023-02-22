a628cfde0236d557bebd2b926c9c3f72e4af08e4
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-02-21 19:29:10 -0500

    se agrego CreateAccount

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,6 +7,7 @@ import RecoveryPassword from '../pages/RecoveryPassword.jsx';
 import Home from '../pages/Home.jsx';
 import NotFound from '../pages/NotFound.jsx';
 import '../styles/global.css';
+import CreateAccount from '../pages/CreateAccount.jsx';
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/new-password" element = {<NewPassword />} />
                     <Route exact path="/login" element = {<Login />} />
+                    <Route exact path="/create-account" element = {<CreateAccount />} />
                     <Route exact path="/recovery-password" element={<RecoveryPassword />} />
                     <Route path= "*" element={<NotFound />} />
                 </Routes>
