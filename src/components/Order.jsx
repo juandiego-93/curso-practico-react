d678287d71eb29d6233dddc26a6b3cf38d9531a3
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-02-21 22:38:07 -0500

    se agrego Menu y Order

---
+++ src/components/Order.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Order = () => {
+    return (
+        <div class="Order">
+        <p>
+          <span>03.25.21</span>
+          <span>6 articles</span>
+        </p>
+        <p>$560.00</p>
+      </div>
+    );
+}
+
+export default Order;
